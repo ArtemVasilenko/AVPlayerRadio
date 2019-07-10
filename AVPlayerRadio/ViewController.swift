13a71c2a79d440a5468396aaf4e7d70670791c79
ArtemVasilenko <artemvasilenko88@gmail.com>
2019-07-10 20:22:51 +0300

    go

--- AVPlayerRadio/ViewController.swift
+++ AVPlayerRadio/ViewController.swift
@@ -1,10 +1,4 @@
-//
-//  ViewController.swift
-//  AVPlayerRadio
-//
-//  Created by Артем on 7/10/19.
-//  Copyright © 2019 Артем. All rights reserved.
-//
+
 
 import UIKit
 
@@ -12,7 +6,6 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
